ca66be2857848a37bb1ff4ad80d62ce01b5c6413
Majid Parsa <MajidKahaki@gmail.com>
2020-08-18 17:43:52 +0430

    add header

--- Asset-Tracking-SPA/src/app/core/core.module.ts
+++ Asset-Tracking-SPA/src/app/core/core.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponenet } from './layout/header/header.component';
 
 @NgModule(
     {
-        declarations : [ FooterComponent],
-        exports : [FooterComponent]
+        declarations: [
+            HeaderComponenet, 
+            FooterComponent
+        ],
+        exports: [
+            HeaderComponenet, 
+            FooterComponent
+        ]
     }
 )
-    
-export class CoreModule
-{
+
+export class CoreModule {
 
 }
