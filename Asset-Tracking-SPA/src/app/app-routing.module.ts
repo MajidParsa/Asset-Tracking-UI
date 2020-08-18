9790c655ccb4a1fd904d35377d7105641b87d35b
Majid Parsa <MajidKahaki@gmail.com>
2020-08-18 20:08:36 +0430

    add dashboard

--- Asset-Tracking-SPA/src/app/app-routing.module.ts
+++ Asset-Tracking-SPA/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import { routes } from './app-routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ Asset-Tracking-SPA/src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { dashboardRoutes } from './dashboard-routes';
+
+@NgModule({
+    imports: [RouterModule.forChild(dashboardRoutes)],
+    exports: [RouterModule]
+})
+
+export class DashboardRoutingModule{}
