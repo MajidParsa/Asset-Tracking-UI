f56ec5c0dd3601bcf713acff76a6247100df6552
Majid Parsa <MajidKahaki@gmail.com>
2020-08-18 17:16:25 +0430

    add footer

---
+++ Asset-Tracking-SPA/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@NgModule(
+    {
+        declarations : [ FooterComponent],
+        exports : [FooterComponent]
+    }
+)
+    
+export class CoreModule
+{
+
+}
